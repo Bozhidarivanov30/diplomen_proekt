e95da5874ff674ebce125e08b7df7452408c2869
Bozhidar Ivanov <bozhidarivanov20a@gmail.com>
2025-02-24 00:52:03 +0200

    fourth commit

--- barcelona-site/next.config.mjs
+++ barcelona-site/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        "upload.wikimedia.org", // For the Barcelona logo
+        "store.fcbarcelona.com", // For the jersey image
+        "thumblr.uniid.it", // For the scarf image
+        "static.nike.com", // For the ball image
+      ],
+    },
+  };
+  
+  export default nextConfig;
