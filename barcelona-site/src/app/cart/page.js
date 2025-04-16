c10b973e963c88d38599e2bf86fe9e679ee0ba66
Bozhidarivanov30 <bozhidarivanov20a@gmail.com>
2025-04-16 08:13:22 +0300

    Update page.js

--- barcelona-site/src/app/cart/page.js
+++ barcelona-site/src/app/cart/page.js
@@ -78,7 +78,6 @@ export default function CartPage() {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Количката ти е празна</h1>
       {cart.length === 0 ? (
         <p className="mt-4 text-lg">Количката ти е празна.</p>
       ) : (
@@ -98,7 +97,7 @@ export default function CartPage() {
                 onClick={() => handleRemoveFromCart(item)}
                 className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
               >
-                Премахжане от количка
+                Премахване от количка
               </button>
             </div>
           ))}
